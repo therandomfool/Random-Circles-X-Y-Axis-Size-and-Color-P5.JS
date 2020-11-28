8e7586429a74e29e34ad710550329e84740e3d10
therandomfool <mddenver@protonmail.com>
2020-11-27 18:10:58 -0800

    clean up sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,11 @@
-// let size = random(1, 100);
-// let xPos = random(1,599);
-// let yPos = random(1,599);
+
 
 
 
 
 function setup() {
   createCanvas(600, 600);
-  background(100);
+  background(111);
 }
 
 function draw() {
@@ -23,18 +21,7 @@ function draw() {
   // let o = random(0, 1);
   let f = color(a, b, c);
   fill(f);
-  // strokeWeight(1);
-  // let w = random(1, 100);
-  // let h = random(1, 100);
-  // let xPos2 = random(1, 599);
-  // let yPos2 = random(1, 599);
-  // ellipse(xPos2, yPos2, w, [h]);
-  // let a2 = random(0, 255);
-  // let b2 = random(0, 255);
-  // let c2 = random(0, 255);
-  // // let o = random(0, 1);
-  // let f = color(a2, b2, c2);
-  // fill(f);
+ 
   
 
 }
